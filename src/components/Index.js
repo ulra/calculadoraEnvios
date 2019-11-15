1add94a1279548827de2c1bcc2d40ea4af1cd915
Raul <Ra.do.nu20@gmail.com>
2019-11-15 15:24:27 -0400

    15Noviembree

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Formulario from './Formulario';
+import Stepper from '@material-ui'
 
 class Index extends Component {
     render() {
@@ -9,15 +10,9 @@ class Index extends Component {
                 <Row center="md">
 
                     <Col xs={12} md={6}>
-                        <h2>Parametros</h2>
-                        <Formulario />
-                    </Col>
-                          
 
-                    <Col xs={12} md={6}>
-                        <h1>total a pagar</h1>
                     </Col>
-                    
+                            
                 </Row>
             </Grid>
         );
