182a388d82c1cef17239224385913f524a462bae
Raul <ra.do.nu20@gmail.com>
2019-11-17 18:22:09 -0400

    17Noviembre

--- src/components/Pagar.js
+++ src/components/Pagar.js
@@ -15,14 +15,20 @@ function descripcion(desc, precio) {
     return { desc, precio };
 }
 
+function TotalArt(items) {
+    return items.map(({ precio }) => precio).reduce((sum, i) => sum + i, 0);
+}
+
 const rows = [
     descripcion('Flete aereo Paq', 10),
-    descripcion('Combustible', 50),
-    descripcion('Seguro', 20),
-    descripcion('Gestion aduanal', 70),
-    descripcion('Impuesto nacionalizacion', 40),
-    descripcion('Transporte costo por paquete', 15),
+    descripcion('Combustible', 10),
+    descripcion('Seguro', 10),
+    descripcion('Gestion aduanal', 10),
+    descripcion('Impuesto nacionalizacion', 10),
+    descripcion('Transporte costo por paquete', 20),
 ];
+
+const invoiceTotalArt = TotalArt(rows);
 
 export default function Pagar() {
         return (
@@ -48,6 +54,16 @@ export default function Pagar() {
                                     <TableCell align="right">{ccyFormat(row.precio)}</TableCell>
                                     </TableRow>
                                 ))}
+
+                                <TableRow>
+                                    <TableCell rowSpan={3} />
+                                    <TableCell colSpan={2}>Total con articulo</TableCell>
+                                    <TableCell align="right">{ccyFormat(invoiceTotalArt)}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Total sin articulo</TableCell>
+                                    <TableCell align="rigth">{ccyFormat(invoiceTotalArt)}</TableCell>
+                                </TableRow>
                             </TableBody>
                         </Paper>
                     </Col>
