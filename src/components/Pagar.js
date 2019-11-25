4dfee19c75394cc0789065656d4c4deeea148207
carlosgomezcp@gmail.com <carlosgomezcp@gmail.com>
2019-11-25 11:47:16 -0400

    fffyf

--- src/components/Pagar.js
+++ src/components/Pagar.js
@@ -3,7 +3,7 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 //import "bootstrap/dist/css/bootstrap.min.css";
-import Table from 'react-bootstrap/Table';
+import Table from '@material-ui/core/Table';
 
 const useStyles = makeStyles(theme =>({
 
